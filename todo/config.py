0e62bc1f73cfbd7a27b55d9c8bc9f94b45f24257
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 20:09:10 +0300

    Misc error

--- todo/config.py
+++ todo/config.py
@@ -1,5 +1,5 @@
 """Ключевые параметры по умолчанию."""
 from pathlib import Path
 
-DB_PATH = Path(__file__).parents[2] / "db" / "ToDo.db"
+DB_PATH = Path(__file__).parents[1] / "db" / "ToDo.db"
 BACKEND_PORT = 5000
