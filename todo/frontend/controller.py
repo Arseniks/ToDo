b0f86d0894b940f43328dab234250e3aa738c3a6
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 10:24:01 +0300

    Callbacks params

--- todo/frontend/controller.py
+++ todo/frontend/controller.py
@@ -1,11 +1,10 @@
 """Обрабока результатов взаимодействия пользователя с интерфейсом."""
 import dash
-import dash_html_components as html
 import requests
 import uvicorn
 from dash.dependencies import Input
 from dash.dependencies import Output
-from dash.exceptions import PreventUpdate
+from dash.dependencies import State
 
 from todo.frontend import view
 
@@ -33,31 +32,58 @@ def data_and_selected(endpoint):
 
 
 @app.callback(
-    [
-        Output(component_id="Today", component_property="children"),
-        Output(component_id="Overdue", component_property="children"),
-        Output(component_id="Pending", component_property="children"),
-        Output(component_id="All", component_property="children"),
-    ],
+    Output(component_id="Data", component_property="children"),
     [Input(component_id="Tabs", component_property="value")],
 )
-def load_data(value):
-    """Загрузка данных для вкладки All."""
-    if value is None:
-        tables = []
-        for name in ["Today", "Overdue", "Pending", "All"]:
-            data, selected = data_and_selected(name.lower())
-            if data:
-                table = view.Table(name, data, selected)
-            else:
-                table = html.H2(f"No todo", style={"marginLeft": 6}, )
-            tables.append(table)
-        return tables
-    raise PreventUpdate
+def show_data(tab_name):
+    """Загружает и отображает данные.
+
+    При наличии сосздается таблица ToDo, а при отсутсвии сообщение об отсутсвии.
+    """
+    data, selected = data_and_selected(tab_name.lower())
+    if data:
+        table = view.TableToDo(data, selected)
+    else:
+        table = view.NoToDo()
+    return table
+
+
+@app.callback(
+    Output(component_id="Table", component_property="sort_action"),
+    [Input(component_id="Table", component_property="selected_row_ids")],
+    [State(component_id="Table", component_property="data"), ],
+)
+def toggle_todo(row_ids, data):
+    """Сохраняет на сервер изменение флага завершенности дела."""
+    print(row_ids, data)
+    return "native"
+
+
+@app.callback(
+    [
+        Output(component_id="Name", component_property="value"),
+        Output(component_id="Date", component_property="date"),
+        Output(component_id="Description", component_property="value"),
+        Output(component_id="Tabs", component_property="value"),
+    ],
+    [Input(component_id="Button", component_property="n_clicks")],
+    [
+        State(component_id="Name", component_property="value"),
+        State(component_id="Date", component_property="date"),
+        State(component_id="Description", component_property="value"),
+        State(component_id="Tabs", component_property="value"),
+    ],
+)
+def add_todo(_, name, date, description, tab_name):
+    """Добавляет ToDo в базу.
+
+    Стирает информацию в диалоге и обнавляет отбражение данных.
+    """
+    print(_, name, date, description, tab_name)
+    return "", date, "", tab_name
 
 
 if __name__ == "__main__":
     uvicorn.run(f"{__name__}:app.server", interface="wsgi", host="0.0.0.0")
 
-    # Постановка галочки (4 варианта) selected_rows
-    # Переключение вкладок value
+    # Редактирование???
--- todo/frontend/view.py
+++ todo/frontend/view.py
@@ -9,35 +9,44 @@ class TaskManager(html.Div):
     """HTML представление приложения по управлению ToDO."""
 
     def __init__(self):
+        super().__init__(
+            [html.H1("Tasks manager"), Tabs(), html.Div(id="Data"), Dialog(), ]
+        )
+
+
+class Tabs(dcc.Tabs):
+    """Набор вкладок с разными представленими дел."""
+
+    def __init__(self):
         tabs = [
             Tab("Today"),
             Tab("Overdue"),
             Tab("Pending"),
             Tab("All"),
         ]
-
-        super().__init__(
-            [
-                html.H1("Tasks manager"),
-                dcc.Tabs(id="Tabs", vertical=True, children=tabs, value=None),
-                Dialog(),
-            ]
-        )
+        super().__init__(id="Tabs", vertical=False, children=tabs, value="Today")
 
 
 class Tab(dcc.Tab):
     """Вкладка с таблицей с существующими ToDo."""
 
     def __init__(self, name):
-        super().__init__(id=name, label=name, value=name)
+        super().__init__(label=name, value=name)
 
 
-class Table(dt.DataTable):
+class NoToDo(html.H2):
+    """Сообщение об остсутсвии ToDo в данном представлении.."""
+
+    def __init__(self):
+        super().__init__("No todo", style={"marginLeft": 6})
+
+
+class TableToDo(dt.DataTable):
     """Таблица с существующими ToDo."""
 
-    def __init__(self, name, data, selected_rows):
+    def __init__(self, data, selected_rows):
         super().__init__(
-            id=f"T_{name}",
+            id=f"Table",
             columns=[
                 {"name": "Name", "id": "name", "type": "text"},
                 {"name": "Date", "id": "date", "type": "datetime"},
@@ -49,22 +58,22 @@ class Table(dt.DataTable):
             sort_action="native",
             row_selectable="multi",
             style_cell={"textAlign": "left", "whiteSpace": "normal"},
-            style_table={"overflowY": "auto", "marginLeft": 6},
+            style_table={"overflowY": "auto", "marginLeft": 6, "marginTop": 18},
             style_as_list_view=True,
         )
 
 
 class Dialog(html.Div):
-    """Диалог для ввода нового ToDO."""
+    """Диалог для ввода нового ToDo."""
 
     def __init__(self):
         super().__init__(
             children=[
                 html.H2("Add task"),
-                dcc.Input(id="input", style={"marginTop": 0}, placeholder="Name"),
-                dcc.DatePickerSingle(id="date", date=date.today(), display_format="YYYY-MM-DD"),
-                dcc.Textarea(id="text", placeholder="Description", style={"height": 100}),
-                html.Button(children="ADD", id="button"),
+                dcc.Input(id="Name", style={"marginTop": 0}, placeholder="Name"),
+                dcc.DatePickerSingle(id="Date", date=date.today(), display_format="YYYY-MM-DD"),
+                dcc.Textarea(id="Description", placeholder="Description", style={"height": 100}),
+                html.Button(id="Button", children="ADD", ),
             ],
             style={"display": "flex", "flexFlow": "column wrap", "maxWidth": 480},
         )
