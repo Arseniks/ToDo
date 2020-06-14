fa37f77f7f03ccd8efc9b03d89961b8e6482955e
Anton <lutsenko.anton@icloud.com>
2020-06-14 20:48:01 +0300

    fixed cors error

--- todo/backend/__main__.py
+++ todo/backend/__main__.py
@@ -3,12 +3,24 @@ from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from todo.backend.config import PORT
 from todo.backend.endpoints import router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def main():
     app.include_router(router)
